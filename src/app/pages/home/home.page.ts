6b65053abcc35f0fc655efaeececa1b5971f0e87
VlV <email@email.com>
2022-09-12 23:10:46 -0500

    Feat -> Componetiza home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+export interface HomeModel {
+  nombre: string;
+  router: string;
+  icon: string;
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  objData: HomeModel[] = [
+    {
+      nombre: 'lactancia',
+      router: '/lactancia',
+      icon: 'restaurant-outline',
+    },
+    {
+      nombre: 'aseo',
+      router: '/aseo',
+      icon: 'fish-outline',
+    },
+    {
+      nombre: 'dormir',
+      router: '/dormir',
+      icon: 'moon-outline',
+    },
+    {
+      nombre: 'juego',
+      router: '',
+      icon: 'football-outline',
+    },
+    {
+      nombre: 'congelado',
+      router: '',
+      icon: 'snow-outline',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
